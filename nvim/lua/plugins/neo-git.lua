b77f1f3b5ff7c81822f6eb0d0eaf876d599a48a1
HuyVo <bmhuyquoc104@gmail.com>
2024-10-06 21:00:01 +0700

    Remove unnecessary themes

--- nvim/lua/plugins/neo-git.lua
+++ nvim/lua/plugins/neo-git.lua
@@ -1,10 +1,19 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
-    "ibhagwan/fzf-lua",              -- optional
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "ibhagwan/fzf-lua",
   },
-  config = true
-}
+  config = function()
+    local neogit = require("neogit")
+    neogit.setup({})
 
+    vim.keymap.set("n", "<leader>gov", function()
+      neogit.open({ kind = "vsplit" })
+    end, { desc = "Neogit open vertical" })
+    vim.keymap.set("n", "<leader>goh", function()
+      neogit.open({ kind = "split_below" })
+    end, { desc = "Neogit open split below" })
+  end,
+}
