98c4c7432e8f5d6fbce5dde67b73840116cf83de
HuyVo <bmhuyquoc104@gmail.com>
2024-10-11 02:35:24 +0700

    Add more shortcut for Fzf

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,12 +1,14 @@
 return {
 	"ibhagwan/fzf-lua",
-	-- optional for icon support
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		-- calling `setup` is optional for customization
 		require("fzf-lua").setup({
-			vim.keymap.set("n", "<leader>gfs", require("fzf-lua").git_files, { desc = "Fzf Git Files" }),
-			vim.keymap.set("n", "<leader>gbs", require("fzf-lua").git_branches, { desc = "Fzf Git Branches" }),
+			vim.keymap.set("n", "<leader>sgf", require("fzf-lua").git_files, { desc = "Fzf Git Files" }),
+			vim.keymap.set("n", "<leader>sgb", require("fzf-lua").git_branches, { desc = "Fzf Git Branches" }),
+			vim.keymap.set("n", "<leader>scp", require("fzf-lua").live_grep, { desc = "Fzf Current Project" }),
+			vim.keymap.set("n", "<leader>sws", require("fzf-lua").lsp_live_workspace_symbols, { desc = "Fzf Git Branches" }),
+			vim.keymap.set("n", "<leader>sif", require("fzf-lua").blines, { desc = "Fzf In files" }),
+			vim.keymap.set("n", "<leader>sb", require("fzf-lua").buffers, { desc = "Fzf buffers" }),
 		})
 	end,
 }
