3fc4132c61fbab700bf471e430e353d8edba1ed7
huyvq <huy.vo2@employmenthero.com>
2024-10-22 18:22:35 +0700

    Add new plugins

--- nvim/lua/config/custom-commands.lua
+++ nvim/lua/config/custom-commands.lua
@@ -1,20 +1,14 @@
-vim.api.nvim_create_user_command(
-  'Rspec',
-  function(opts)
-    -- Use vim.fn.expand to get the current file if no argument is passed
-    local test_path
-    if opts.args ~= "" then
-      test_path = opts.args  -- Use the argument passed to the command
-    else
-      test_path = vim.api.nvim_call_function("expand", {"%"})  -- Get the current file
-    end
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
 
-    -- Run the test for the specified path with a callback to open output after test completes
-    require("neotest").run.run(test_path, function()
-      -- This function will be called once the test is completed
-      require("neotest").output.open({ enter = true })
-    end)
-  end,
-  { nargs = "?" } -- Allow either zero or one argument
-)
-
+vim.api.nvim_set_keymap("n", "bl", ":blast<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "bf", ":bfirst<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "bp", ":bprev<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "bn", ":bnext<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "bd", ":bdelete<enter>", { noremap = false })
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,13 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      auto_install = true,
-      indent = { enable = true },
-      highlight = { enable = true},
-    })
-  end
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        auto_install = true,
+        indent = { enable = true },
+        highlight = { enable = true },
+      })
+    end,
+  },
 }
