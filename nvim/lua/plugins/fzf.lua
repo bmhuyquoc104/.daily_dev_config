fddcc282bd6c641576e608c225a4a5cd92a146bf
huyvq <huy.vo2@employmenthero.com>
2024-11-26 16:29:41 +0700

    Update option for fzf

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -2,20 +2,35 @@ return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+    local fzf = require("fzf-lua")
+
+    local function search_in_directory_or_root()
+      local input = vim.fn.input("Directory (leave empty for root): ")
+      local search_dir = input ~= "" and input or vim.loop.cwd()
+      fzf.live_grep({ cwd = search_dir })
+    end
+
     require("fzf-lua").setup({
-      vim.keymap.set("n", "<leader>sgf", require("fzf-lua").git_files, { desc = "Fzf Git Files" }),
-      vim.keymap.set("n", "<leader>sgb", require("fzf-lua").git_branches, { desc = "Fzf Git Branches" }),
-      vim.keymap.set("n", "<leader>scp", require("fzf-lua").live_grep, { desc = "Fzf Current Project" }),
+      vim.keymap.set("n", "<leader>sgf", fzf.git_files, { desc = "Fzf Git Files" }),
+      vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "Fzf Files" }),
+      vim.keymap.set("n", "<leader>sgb", fzf.git_branches, { desc = "Fzf Git Branches" }),
+      vim.keymap.set("n", "<leader>scp", fzf.live_grep, { desc = "Fzf Current Project" }),
       vim.keymap.set(
         "n",
         "<leader>sws",
-        require("fzf-lua").lsp_live_workspace_symbols,
+        fzf.lsp_live_workspace_symbols,
         { desc = "Fzf Live Workspace Symbols" }
       ),
-      vim.keymap.set("n", "<leader>sif", require("fzf-lua").blines, { desc = "Fzf In files" }),
-      vim.keymap.set("n", "<leader>sb", require("fzf-lua").buffers, { desc = "Fzf buffers" }),
-      vim.keymap.set("n", "<leader>sh", require("fzf-lua").helptags, { desc = "Fzf Help tags" }),
-      vim.keymap.set("n", "<leader>sr", require("fzf-lua").registers, { desc = "Fzf registers" }),
+      vim.keymap.set("n", "<leader>sif", fzf.blines, { desc = "Fzf In files" }),
+      vim.keymap.set("n", "<leader>sb", fzf.buffers, { desc = "Fzf buffers" }),
+      vim.keymap.set("n", "<leader>sh", fzf.helptags, { desc = "Fzf Help tags" }),
+      vim.keymap.set("n", "<leader>sr", fzf.registers, { desc = "Fzf registers" }),
+      vim.keymap.set(
+        "n",
+        "<leader>sd",
+        search_in_directory_or_root,
+        { desc = "Fzf Search in Directory or Root" }
+      ),
     })
   end,
 }
