63876c3d49b467cc9085bd50978fca5f1053a7f0
HuyVo <bmhuyquoc104@gmail.com>
2025-02-11 09:29:15 +0700

    Leverage diffview

--- nvim/lua/plugins/neo-git.lua
+++ nvim/lua/plugins/neo-git.lua
@@ -1,24 +1,54 @@
 return {
-	"NeogitOrg/neogit",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"sindrets/diffview.nvim",
-	},
-  cmd = { "Neogit" },
-	keys = {
-		{
-			"<leader>gov",
-			"<cmd>Neogit kind=vsplit<CR>",
-			desc = "Neogit open vertical",
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
 		},
-    {
-      "<leader>gos",
-      "<cmd> Neogit kind=split_below<CR>",
-      desc = "Neogit open split below"
-    }
+		cmd = { "Neogit" },
+		keys = {
+			{
+				"<leader>gov",
+				"<cmd>Neogit kind=vsplit<CR>",
+				desc = "Neogit open vertical",
+			},
+			{
+				"<leader>gos",
+				"<cmd> Neogit kind=split_below<CR>",
+				desc = "Neogit open split below",
+			},
+		},
+		config = function()
+			local neogit = require("neogit")
+			neogit.setup({})
+		end,
 	},
-	config = function()
-		local neogit = require("neogit")
-		neogit.setup({})
-	end,
+	{
+		"sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen" },
+    keys = {
+      {
+        "<leader>dvo",
+        "<cmd>DiffviewOpen<CR>",
+        desc = "Diffview open merge tool",
+      },
+    },
+		config = function()
+			require("diffview").setup({
+				view = {
+					default = {
+						layout = "diff2_horizontal",
+					},
+					file_history = {
+						layout = "diff2_horizontal",
+					},
+					diff_view = {
+						layout = "diff2_horizontal",
+					},
+					merge_tool = {
+						layout = "diff1_plain",
+					},
+				},
+			})
+		end,
+	},
 }
