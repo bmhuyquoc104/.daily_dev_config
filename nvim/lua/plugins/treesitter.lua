6794cc1e9a7258c60e503e1da7735c3685d82b08
HuyVo <bmhuyquoc104@gmail.com>
2025-04-30 01:26:26 +0700

    Update treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
+        ensure_installed = { "yaml" },
 				auto_install = true,
 				indent = { enable = true },
 				highlight = { enable = true, additional_vim_regex_highlighting = false },
