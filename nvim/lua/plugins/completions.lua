83e83fb84c6198d6237a08383bdf03b2cf6447ee
HuyVo <bmhuyquoc104@gmail.com>
2025-05-01 12:12:33 +0700

    Add vim-dadbod for manage db inside nvim

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,35 +1,39 @@
 return {
-	{
-		"saghen/blink.cmp",
-		dependencies = {
-			{ "fang2hou/blink-copilot" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-		version = "*",
-		opts = {
-			keymap = { preset = "default" },
-			appearance = {
-				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				nerd_font_variant = "mono",
-				kind_icons = {
-					Copilot = "",
-				},
-			},
-			sources = {
-				default = { "copilot", "lsp", "path", "snippets", "buffer" },
-				providers = {
-					copilot = {
-						name = "copilot",
-						module = "blink-copilot",
-						score_offset = 100,
-						async = true,
-						opts = {
-							max_completions = 3,
-							max_attempts = 4,
-						},
-					},
-				},
-			},
-		},
-	},
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      { "fang2hou/blink-copilot" },
+      { "rafamadriz/friendly-snippets" },
+    },
+    version = "*",
+    opts = {
+      keymap = { preset = "default" },
+      appearance = {
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        nerd_font_variant = "mono",
+        kind_icons = {
+          Copilot = "",
+        },
+      },
+      sources = {
+        default = { "copilot", "lsp", "path", "snippets", "buffer" },
+        per_filetype = {
+          sql = { "snippets", "dadbod", "buffer" },
+        },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+            opts = {
+              max_completions = 3,
+              max_attempts = 4,
+            },
+          },
+          dadbod = { module = "vim_dadbod_completion.blink" },
+        },
+      },
+    },
+  },
 }
