d96e0d049f11839d947dc39d2bad8eb51b794618
HuyVo <bmhuyquoc104@gmail.com>
2025-10-12 01:18:56 +0700

    Support copy diagnostic message

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -8,11 +8,9 @@ return {
 	{
 		"windwp/nvim-ts-autotag",
 		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			enable_close = true,
-			enable_rename = true,
-			enable_close_on_slash = false,
-		},
+		config = function()
+			require("nvim-ts-autotag").setup({})
+		end,
 	},
 	{
 		"MagicDuck/grug-far.nvim",
@@ -43,11 +41,14 @@ return {
 			grug_far.setup({})
 		end,
 	},
+
 	{
 		"rachartier/tiny-inline-diagnostic.nvim",
 		event = "LspAttach",
 		config = function()
-			require("tiny-inline-diagnostic").setup({
+			local tiny_diag = require("tiny-inline-diagnostic")
+
+			tiny_diag.setup({
 				preset = "ghost",
 				transparent_bg = true,
 				options = {
@@ -58,7 +59,43 @@ return {
 					},
 				},
 			})
+
 			vim.diagnostic.config({ virtual_text = false })
+
+			local function copy_diagnostic_under_cursor()
+				local bufnr = vim.api.nvim_get_current_buf()
+				local cursor = vim.api.nvim_win_get_cursor(0)
+				local cursor_line = cursor[1] - 1
+				local cursor_col = cursor[2]
+				local diagnostics = vim.diagnostic.get(bufnr)
+
+				local found = nil
+				for _, d in ipairs(diagnostics) do
+					if
+						cursor_line >= d.lnum
+						and cursor_line <= (d.end_lnum or d.lnum)
+						and cursor_col >= (d.col or 0)
+						and cursor_col <= (d.end_col or d.col or 0)
+					then
+						found = d
+						break
+					end
+				end
+
+				if found and found.message then
+					vim.fn.setreg("+", found.message)
+					vim.notify("Copied diagnostic: " .. found.message, vim.log.levels.INFO)
+				else
+					vim.notify("No diagnostic at cursor position", vim.log.levels.WARN)
+				end
+			end
+
+			vim.keymap.set(
+				"n",
+				"<leader>teo",
+				copy_diagnostic_under_cursor,
+				{ desc = "Copy diagnostic message under cursor" }
+			)
 		end,
 	},
 	{
