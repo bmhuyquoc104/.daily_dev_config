c73ccf87b3adb58aa3cddf545a2e59f3bce6c270
Huy.Vo@modec.com <huy.vomodec.com@SGP-HUY-MAC.local>
2025-11-06 13:32:10 +0700

    Add more custom commands for file

--- nvim/lua/config/custom-commands.lua
+++ nvim/lua/config/custom-commands.lua
@@ -1,11 +1,11 @@
 -- Highlight yanked text
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
 
 -- Custom buffer keyboard shortcuts
@@ -17,3 +17,19 @@ vim.api.nvim_set_keymap("n", "bd", ":bdelete<enter>", { noremap = false })
 vim.keymap.set("n", "tn", ":tabnew<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "tc", ":tabclose<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "<leader>p", '"d"+P', { noremap = false })
+vim.keymap.set("n", "<leader>cf", function()
+	vim.fn.setreg("+", vim.fn.expand("%:t"))
+	vim.notify("Copied file name: " .. vim.fn.expand("%:t"))
+end, { noremap = true, silent = true, desc = "Copy file name" })
+
+vim.keymap.set("n", "<leader>cd", function()
+	vim.fn.setreg("+", vim.fn.expand("%:p:h"))
+	vim.notify("Copied directory: " .. vim.fn.expand("%:p:h"))
+end, { noremap = true, silent = true, desc = "Copy directory path" })
+
+vim.keymap.set("n", "<leader>cfp", function()
+	vim.fn.setreg("+", vim.fn.expand("%:p"))
+	vim.notify("Copied full path: " .. vim.fn.expand("%:p"))
+end, { noremap = true, silent = true, desc = "Copy full file path" })
+
+
