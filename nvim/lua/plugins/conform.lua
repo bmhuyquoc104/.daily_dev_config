e97a97404d6cd3a7fd707a40e9261222380a1626
Huy.Vo@modec.com <huy.vomodec.com@SGP-HUY-MAC.local>
2026-03-31 14:02:39 +0700

    Add shortcut to toggle conform autoformat on save

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,47 +1,72 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	opts = {
-		format_on_save = {
-			timeout_ms = 1000,
-			lsp_fallback = true,
-		},
+	config = function()
+		local conform = require("conform")
 
-		formatters_by_ft = {
-			lua = { "stylua" },
+		conform.setup({
+			format_on_save = function(bufnr)
+				if vim.b[bufnr].disable_autoformat then
+					return
+				end
 
-			python = { "ruff_organize_imports", "ruff_format" },
+				return {
+					timeout_ms = 1000,
+					lsp_fallback = true,
+				}
+			end,
 
-			ruby = { "rubocop" },
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff_organize_imports", "ruff_format" },
+				ruby = { "rubocop" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
 
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			html = { "prettier" },
-			css = { "prettier" },
-			json = { "prettier" },
-			yaml = { "prettier" },
-			markdown = { "prettier" },
+				sql = { "sqlfluff" },
+				mysql = { "sqlfluff" },
+				pgsql = { "sqlfluff" },
 
-			sql = { "sqlfluff" },
-			mysql = { "sqlfluff" },
-			pgsql = { "sqlfluff" },
-
-			sh = { "shfmt" },
-			bash = { "shfmt" },
-			zsh = { "shfmt" },
-		},
-
-		formatters = {
-			sqlfluff = {
-				command = "sqlfluff",
-				args = {
-					"fix",
-					"--dialect",
-					"ansi",
-					"$FILENAME",
-				},
-				stdin = false,
+				sh = { "shfmt" },
+				bash = { "shfmt" },
+				zsh = { "shfmt" },
 			},
-		},
-	},
+
+			formatters = {
+				sqlfluff = {
+					command = "sqlfluff",
+					args = {
+						"fix",
+						"--dialect",
+						"ansi",
+						"$FILENAME",
+					},
+					stdin = false,
+				},
+			},
+		})
+
+		-- default: enabled
+		vim.b.disable_autoformat = false
+
+		-- toggle command (buffer-local)
+		vim.api.nvim_create_user_command("FormatToggle", function()
+			vim.b.disable_autoformat = not vim.b.disable_autoformat
+
+			if vim.b.disable_autoformat then
+				vim.notify("Autoformat (buffer): OFF")
+			else
+				vim.notify("Autoformat (buffer): ON")
+			end
+		end, {})
+
+		vim.keymap.set("n", "<leader>ct", "<cmd>FormatToggle<CR>", {
+			desc = "Toggle autoformat (buffer)",
+		})
+	end,
 }
