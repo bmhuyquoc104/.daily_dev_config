61b3845106af5bbba9b05630503b0aefde115f7d
HuyVo <bmhuyquoc104@gmail.com>
2026-04-05 12:09:00 +0700

    use fast animation

--- nvim/lua/plugins/animation.lua
+++ nvim/lua/plugins/animation.lua
@@ -1,7 +1,7 @@
 return {
 	"sphamba/smear-cursor.nvim",
 	opts = { -- Default  Range
-		stiffness = 0.8, -- 0.6      [0, 1]
+		stiffness = 0.9, -- 0.6      [0, 1]
 		trailing_stiffness = 0.6, -- 0.45     [0, 1]
 		stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
 		trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
